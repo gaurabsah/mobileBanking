1cb963de955d66269bca68f178c25cb821124961
Gaurab Sah <gaurab.sah.777@gmail.com>
2024-10-09 22:22:05 +0530

    initial commit

---
+++ src/main/java/com/bank/mobileBanking/MobileBankingApplication.java
@@ -0,0 +1,13 @@
+package com.bank.mobileBanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/mobileBanking/MobileBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.mobileBanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
